4631a8a2e70f7cc83db63cd00e963218f12d4a5b
DuroMonstahCardo <efren.garcia6969@gmail.com>
2025-03-21 09:32:16 -0600

    Revert "redux initial setup"
    This reverts commit f8c8dc68b7672cecb0c06deaa1ec1e1b27c04b09.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AppProvider from "../../redux/providers/AppProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppProvider>
-          {children}
-        </AppProvider>
+        {children}
       </body>
     </html>
   );
