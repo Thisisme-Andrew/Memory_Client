95d529a80dd8e3d43a19fe47c9d6a8820773cbd7
Thisisme-Andrew <thisemailstotroll@gmail.com>
2025-03-31 13:31:10 -0600

    documentation updates

--- services/images/imagesServices.js
+++ services/images/imagesServices.js
@@ -1,23 +1,5 @@
-import { BlobServiceClient } from "@azure/storage-blob";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.NEXT_PUBLIC_AZURE_BLOB_CONNECTION_STRING);
-
-// export const createContainer = async (containerName) =>  {
-//   const containerClient = blobServiceClient.getContainerClient(containerName);
-//   await containerClient.createIfNotExists();
-// }
-
-// export const listContainers = async () => {
-//   let containers = blobServiceClient.listContainers();
-//   for await (const container of containers) {
-//       console.log(`Container Name: ${container.name}`);
-//   }
-//   return containers;
-// }
-
+// This returns URL ofimage
+// Just need to give it a file to input of type jpeg, png, jpg (maybe a few others but typical image file types)
 export const uploadBlob = async (file) => {
   const containerURL = process.env.NEXT_PUBLIC_AZURE_BASE_SAS_URL;
   const blobName = `${Date.now()}-${file.name}`;
@@ -44,36 +26,4 @@ export const uploadBlob = async (file) => {
       console.log("Error uploading to Azure:", err);
       throw err;
   }
-
-  
-  // const containerClient = blobServiceClient.getContainerClient(containerName);
-  
-  // await containerClient.createIfNotExists();
-
-  // const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  // const stream = Readable.from(file.buffer);
-  // const uploadOptions = { blobHTTPHeaders: { blobContentType: file.mimetype } };
-  
-  // await blockBlobClient.uploadStream(stream, file.buffer.length, undefined, uploadOptions);
-  // console.log(`File uploaded successfully. Request ID: ${uploadBlobResponse.requestId}`);
-  
 }
-
-// export const downloadBlob = async (containerName, blobName) =>  {
-//   const containerClient = blobServiceClient.getContainerClient(containerName);
-//   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-//   const downloadBlockBlobResponse = await blockBlobClient.downloadToFile("./downloaded-file.jpeg");
-//   console.log(`Downloaded successfully to "downloaded-file.jpeg".`);
-// }
-
-// export const deleteContainer = async (containerName) => {
-//   const containerClient = blobServiceClient.getContainerClient(containerName);
-
-//   try {
-//     await containerClient.delete();
-//     console.log(`Container '${containerName}' deleted successfully.`);
-//   } catch (error) {
-//     console.error('Error deleting container (' + containerName + '): ', error.message);
-//   }
-// }
